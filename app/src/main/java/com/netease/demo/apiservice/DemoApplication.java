02cde002a3004b6851ba10d6a8cf976ba8323ed5
张云龙 <hzzhangyunlong@corp.netease.com>
2018-10-25 09:52:42 +0800

    other(1.0.0): 小修改

--- app/src/main/java/com/netease/demo/apiservice/DemoApplication.java
+++ app/src/main/java/com/netease/demo/apiservice/DemoApplication.java
@@ -2,6 +2,8 @@ package com.netease.demo.apiservice;
 
 import android.app.Application;
 
+import com.netease.demo.apiservice.api_service.ApiRegister;
+
 /**
  * Created by zyl06 on 2018/7/30.
  */
@@ -11,5 +13,6 @@ public class DemoApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        ApiRegister.init();
     }
 }
